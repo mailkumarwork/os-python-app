ccbfc50bec022e843454c9e612abb1f3a88cd76f
Taseer Ahmed <taseer94@gmail.com>
2018-06-03 04:57:21 +0500

    A simple Flask application. First commit

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+workers = int(os.environ.get("GUNICORN_PROCESSES", '3'))
+threads = int(os.environ.get("GUNICORN_THREADS", '1'))
+
+forwarded_allow_apis = "*"
+secure_schema_headers = {'X-Forwarded-Proto': 'https'}
